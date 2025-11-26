b81412382ef992eb46a796f5815d78951a0732d9
Ronit <ronitbajaj05@gmail.com>
2025-11-26 12:21:17 +0530

    Removed unused imports

--- BACKEND/src/main/java/com/anand/backend/controller/MovieController.java
+++ BACKEND/src/main/java/com/anand/backend/controller/MovieController.java
@@ -13,8 +13,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
--- BACKEND/src/main/java/com/anand/backend/repository/MovieRepository.java
+++ BACKEND/src/main/java/com/anand/backend/repository/MovieRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 
 @Repository
